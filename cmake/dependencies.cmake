5ba14290d027681cfdccede301fe85433d1e345a
JanSimplify <101509908+JanSimplify@users.noreply.github.com>
2025-03-10 22:36:33 +0800

    build: KRCMake升级为v1.0.0

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -5,8 +5,8 @@ include(FetchContent)
 
 FetchContent_Declare(
     KRCMake
-    GIT_REPOSITORY https://github.com/JanSimplify/KRCMake.git
-    GIT_TAG a195d00
+    URL https://github.com/JanSimplify/KRCMake/archive/refs/tags/v1.0.0.zip
+    URL_HASH MD5=553a4ca12751ddb5dba2405120c5bb82
 )
 
 FetchContent_MakeAvailable(KRCMake)
